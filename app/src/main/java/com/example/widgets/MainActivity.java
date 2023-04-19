0a5683ae17a1b5670e1aca6f0ccd86e9d621d30d
a21malfo <a21@malfo@student.his.se>
2023-04-19 12:49:26 +0200

    Update with a soundeffect to the button widget

--- app/src/main/java/com/example/widgets/MainActivity.java
+++ app/src/main/java/com/example/widgets/MainActivity.java
@@ -2,13 +2,26 @@ package com.example.widgets;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+        final MediaPlayer mp = MediaPlayer.create(this, R.raw.barking);
+        Button Button = (Button) this.findViewById(R.id.Button);
+        Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mp.start();
+            }
+        });
+
+     }
 }
